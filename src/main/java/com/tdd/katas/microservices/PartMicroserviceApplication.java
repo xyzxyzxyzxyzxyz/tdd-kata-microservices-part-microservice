77b160a6548a17341fe76c4f80ed1ca42ce56fd5
XYZ <idm.caz@mailinator.com>
2017-04-09 16:13:50 +0200

    Initial commit

---
+++ src/main/java/com/tdd/katas/microservices/PartMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.tdd.katas.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartMicroserviceApplication.class, args);
+	}
+}
